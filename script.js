15b673be11777e3e3b3deafad49e8c69fdee5a64
Saqib Nasir <msaqibtech1@gmail.com>
2025-01-09 11:46:52 +0500

    todo app

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+// add task 
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You May Write some task or list in search box..");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+
+// select or remove 
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+inputBox.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTask(inputBox.value);
+    }
+});
+
+
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
